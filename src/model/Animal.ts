0c9fb03c91620e1aebdba488b05cda866d8c4a4c
Kodeneko <ladysun.freedom@gmail.com>
2023-08-08 23:01:55 +0200

    [db] added species mdoel

--- src/model/Animal.ts
+++ src/model/Animal.ts
@@ -1,7 +1,9 @@
 import EnumSex from "./EnumSex";
+import EnumSpecies from "./EnumSpecies";
 
 interface Animal {
   name: string;
+  species: EnumSpecies;
   sex: EnumSex;
   breed: string;
   bday: Date;
