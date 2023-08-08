9ff731021ea16d237db0ae98a6bfbb9ffe7dc6d2
Kodeneko <ladysun.freedom@gmail.com>
2023-08-08 11:36:16 +0200

    [db] model created

---
+++ src/model/Animal.ts
@@ -0,0 +1,12 @@
+import EnumSex from "./EnumSex";
+
+interface Animal {
+  name: string;
+  sex: EnumSex;
+  breed: string;
+  bday: Date;
+  enter: Date;
+  desc: string;
+}
+
+export default Animal;
