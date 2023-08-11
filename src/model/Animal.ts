23ec5f1311e2a353022b140e385fab4ccb12ef55
Kodeneko <ladysun.freedom@gmail.com>
2023-08-11 21:39:07 +0200

    [ctrl] changes date to string in animal

--- src/model/Animal.ts
+++ src/model/Animal.ts
@@ -2,12 +2,13 @@ import EnumSex from "./EnumSex";
 import EnumSpecies from "./EnumSpecies";
 
 interface Animal {
+  id?: string;
   name: string;
   species: EnumSpecies;
   sex: EnumSex;
   breed: string;
-  bday: Date;
-  enter: Date;
+  bday: string;
+  enter: string;
   desc: string;
 }
 
