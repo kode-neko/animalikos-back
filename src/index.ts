460443a9117a974ba4a9909035d26defb3035d6d
Kodeneko <ladysun.freedom@gmail.com>
2023-08-13 22:14:56 +0200

    [ctrl] organize init db & server

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
+import mongoose from 'mongoose';
 import Koa from 'koa';
 import koaBody from 'koa-body';
 import {animalRouter} from './router';
 import { errorHandlerMw } from './middleware';
 
-const app: Koa = new Koa();
-app.use(koaBody());
+mongoose.connect('mongodb://localhost:3010/?authSource=animalikos')
+  .then(() => {
+    const app: Koa = new Koa();
+    app.use(koaBody());
+    
+    app.use(animalRouter.routes());
+    app.use(animalRouter.allowedMethods());
+    app.on('error', errorHandlerMw);
+    
+    try {
+      app.listen(4000, () => console.log('Server on port 4000'));
+    } catch (err) {
+      console.log('Error launching server');
+    }
+    
+  })
+  .catch(() => {
+    console.log('Error conection DB');
+  });
 
-app.use(animalRouter.routes());
-app.use(animalRouter.allowedMethods());
-app.on('error', errorHandlerMw);
-
-app.listen(4000);
