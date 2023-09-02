5e723a283ef79e5dad14646cce3ec16e335d0dd5
Kodeneko <ladysun.freedom@gmail.com>
2023-09-02 14:49:34 +0200

    [test] mw validAnimal

--- src/middleware/validAnimal.test.ts
+++ src/middleware/validAnimal.test.ts
@@ -9,17 +9,41 @@ describe('validAnimal', () => {
 
   test('Animal is correct', async() => {
     const animal: Animal = createAnimal();
-
     const mockCtx: unknown = {
       request: { body: animal },
       throw: fake()
     };
     const ctx: Context = mockCtx as unknown as Context;
     const next = mock();
-    
     await validAnimal(ctx, next);
     expect(next.once()).toBeTruthy();
   });
 
+  test('Animal is wrong', async() => {
+    const animal: Animal = createAnimal();
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const {name, ...animalAmputed} = animal; 
+    const mockCtx: unknown = {
+      request: { body: animalAmputed },
+      throw: fake()
+    };
+    const ctx: Context = mockCtx as unknown as Context;
+    const next = mock();
+    await validAnimal(ctx, next);
+    expect(next.once().throws()).toBeTruthy();
+  });
+
+  test('Animal with wrong date', async() => {
+    const animal: Animal = createAnimal();
+    const animalWrongDate: unknown = {...animal, bday: ''}; 
+    const mockCtx: unknown = {
+      request: { body: animalWrongDate },
+      throw: fake()
+    };
+    const ctx: Context = mockCtx as unknown as Context;
+    const next = mock();
+    await validAnimal(ctx, next);
+    expect(next.once().throws()).toBeTruthy();
+  });
 
 });
